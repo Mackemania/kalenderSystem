cfaf38e2fd548905e53f2c23bf1434f6f3a40ba7
Markus Matiasson <markus.matiasson@learnet.se>
2017-11-21 14:24:46 +0100

    First

---
+++ src/kalenderSystem/kalenderSystem_main.java
@@ -0,0 +1,11 @@
+package kalenderSystem;
+
+public class kalenderSystem_main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello GIT!");
+
+	}
+
+}
